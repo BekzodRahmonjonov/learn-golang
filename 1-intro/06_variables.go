ccfb85d45e75737335440c5bd149811fe72e78f0
Bekzod <bekzod03233@mail.ru>
2023-07-08 15:13:48 +0500

    feat: Variables

---
+++ 1-intro/06_variables.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+var c, python, javascript, cpp bool // there we created a few variables and we set boolean type for these. Default value of boolean typed variable is false
+
+func main() {
+	var golang int // there we created variable called golang. type of golang is integer. Default value of integer typed variable is 0
+
+	fmt.Println(golang, c, python, javascript, cpp) // 0 false false false false
+}
